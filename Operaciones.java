9df63f37350880aa950035a0bb088999187cf6ab
Unknown <johansebastian.zambranopaez24@gmail.com>
2018-02-13 09:44:25 -0500

    Operaciones Basicas

---
+++ Operaciones.java
@@ -0,0 +1,10 @@
+public class Operaciones{
+	public static void main(String[] arg){
+		System.out.println("\n\nEjercicio N° 1\n\n");
+		int var1=20, var2=50;
+		System.out.println("\nLa suma de las variable es = "+(var1+var2));
+		System.out.println("\nLa multiplicacion de "+var1+" * " +var2+" es = "+(var1*var2));
+		System.out.println("\nLa division de "+var1+" / " +var2+" es = "+(var1/var2));
+		System.out.println("\nLa resta de "+var1+" - " +var2+" es = "+(var1-var2));
+	}
+}
